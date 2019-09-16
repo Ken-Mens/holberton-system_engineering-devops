eda06f8b2311975c7cf9f1c32227c1b69b0912a1
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-16 18:22:31 +0000

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Export to CSV"""
+import requests
+import csv
+from sys import argv
+
+
+def export_to_csv(user_id):
+
+    user_id = argv[1]
+    to_do = 'https://jsonplaceholder.typicode.com/todos/'
+    users = 'https://jsonplaceholder.typicode.com/users/'
+    todo_req = requests.get(to_do, params={'userId': user_id})
+    user_req = requests.get(users, params={'id': user_id})
+
+    todo_l = todo_req.json()
+    user_l = user_req.json()
+
+    name = user_l[0].get('username')
+    n_file = user_id + '.csv'
+    with open(n_file, 'w', newline='') as csv_file:
+        tasks = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in todo_l:
+            tasks.writerow([user_id, name,
+                           task.get('completed'), task.get('title')])
+
+if __name__ == "__main__":
+    export_to_csv(int(argv[1]))
