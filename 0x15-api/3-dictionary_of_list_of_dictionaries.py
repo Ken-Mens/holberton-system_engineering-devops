13ad5f42d5a507b84932f77fdfe25e00319dc776
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-16 20:23:42 +0000

    dictionary of lists of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Dictionary of list"""
+import requests
+import csv
+from sys import argv
+import json
+
+
+def dict_of_list():
+    n_file = 'todo_all_employees.json'
+    to_do = 'https://jsonplaceholder.typicode.com/todos/'
+    users = 'https://jsonplaceholder.typicode.com/users/'
+    todo_req = requests.get(to_do)
+    user_req = requests.get(users)
+
+    todo_l = todo_req.json()
+    user_l = user_req.json()
+
+    workers = {}
+    current_user = {}
+
+    for phil in user_l:
+        user_id = phil.get('id')
+        workers[user_id] = []
+        current_user[user_id] = phil.get('username')
+
+    for task in todo_l:
+        current_task = {}
+        user_id = task.get('userId')
+        current_task['task'] = task.get('title')
+        current_task['completed'] = task.get('completed')
+        current_task['username'] = current_user.get(user_id)
+        workers.get(user_id).append(current_task)
+
+    with open(n_file, 'w', newline='') as jsonfile:
+        json.dump(workers, jsonfile)
+
+
+if __name__ == "__main__":
+    dict_of_list()
