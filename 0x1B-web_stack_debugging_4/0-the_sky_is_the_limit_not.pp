eb27fc8847eefe5a6236ad723424fab51900661b
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-10 19:58:05 +0000

    fixed syntax of path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Increase size so that nginx server can handle larger traffic
 exec { 'sky is the limit size' :
-  command => 'sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/15/9000/g" /etc/default/nginx; sudo service nginx restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
