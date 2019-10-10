0e29c300b7edd77a84163d429898bd461d2cf628
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-10 19:53:02 +0000

    changed size

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Increase size so that nginx server can handle larger traffic
 exec { 'sky is the limit size' :
-  command => 'sed -i "s/15/5000/g" /etc/default/nginx; sudo service nginx restart',
+  command => 'sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
   path    => '/usr/local/bin/:/bin/'
 }
